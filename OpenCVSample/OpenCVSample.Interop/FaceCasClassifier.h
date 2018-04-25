9c62b1ec4413f3affdef6f2242fef3f18bf214e3
kzchen <companions.awareness@gmail.com>
2018-04-24 23:56:46 -0500

    Psi Opencv project

---
+++ OpenCVSample/OpenCVSample.Interop/FaceCasClassifier.h
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#pragma warning(push)
+#pragma warning(disable : 4793)
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core/cvdef.h>
+#include <opencv2/face.hpp>
+#include "CascadeClassifierUnm.h"
+#include "FaceLandmarkUnm.h"
+#pragma warning(pop)
+
+
+namespace Microsoft
+{
+	namespace Psi
+	{
+		namespace Samples
+		{
+			namespace OpenCV
+			{
+				public ref class FaceCasClassifier
+				{
+				public:
+					CascadeClassifierUnm * face_cascade;
+					FaceLandmarkUnm * facemark;
+					// cv::CascadeClassifier* classifier;
+					// cv::face::FacemarkKazemi * facemark;
+					FaceCasClassifier();
+
+				};
+			}
+		}
+	}
+}
