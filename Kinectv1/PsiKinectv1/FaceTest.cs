55ef19b05485b41e7be8f005d98d008929b1c1ce
Willie Wilson <jrw@northwestern.edu>
2018-07-10 17:42:08 -0500

    v1 Kinect component working with outputing streams for color, depth, skeleton, and audio.  Separate component to do face tracking from the first 3 streams

---
+++ Kinectv1/PsiKinectv1/FaceTest.cs
@@ -0,0 +1,35 @@
+﻿namespace PsiKinectv1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using Microsoft.Psi;
+    using Microsoft.Psi.Imaging;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit.FaceTracking;
+    using PsiKinectv1;
+
+    class FaceTest
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looking for faces...");
+            using (var p = Pipeline.Create())
+            {
+                Microsoft.Psi.Kinect.v1.KinectSensor kinectSensor = new Microsoft.Psi.Kinect.v1.KinectSensor(p);
+
+                Microsoft.Psi.Kinect.v1.SkeletonFaceTracker faceTracker = new Microsoft.Psi.Kinect.v1.SkeletonFaceTracker(p, kinectSensor.kinectSensor);
+
+                var joinedFrames = kinectSensor.ColorImage.Join(kinectSensor.DepthImage).Join(kinectSensor.Skeletons);
+
+                joinedFrames.PipeTo(faceTracker);
+
+                faceTracker.FaceDetected.Do(x => Console.WriteLine("Face: " + x));
+
+                p.Run();
+            }
+        }
+
+    }
+}
