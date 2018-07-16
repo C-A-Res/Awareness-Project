ca0af5950d10e72420fa13206b1c75232bcf5032
Willie Wilson <jrw@northwestern.edu>
2018-07-16 12:43:55 -0500

    new pipeline for the kiosk using the kinect v1

---
+++ KioskMain/KinectKioskProgram.cs
@@ -0,0 +1,76 @@
+﻿namespace NU.Kiosk
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.Kinect;
+    using Microsoft.Psi;
+    using Microsoft.Psi.Kinect.v1;
+    using Microsoft.Psi.Imaging;
+    using Microsoft.Psi.Media;
+    using Microsoft.Psi.Audio;
+    using Microsoft.Psi.Data;
+    using Microsoft.Psi.Speech;
+    using Microsoft.Psi.Visualization.Client;
+
+    public static class KinectKioskProgram
+    {
+        static TimeSpan _100ms = TimeSpan.FromSeconds(0.1);
+
+        public static void Main(string[] args)
+        {
+            bool detected = false;
+
+            Console.WriteLine("Starting Kinect-based Kiosk.  Verify that Kinect is setup before continuing");
+
+            using (Pipeline pipeline = Pipeline.Create())
+            {
+                Microsoft.Psi.Kinect.v1.KinectSensor kinectSensor = new Microsoft.Psi.Kinect.v1.KinectSensor(pipeline);
+
+                Microsoft.Psi.Kinect.v1.SkeletonFaceTracker faceTracker = new Microsoft.Psi.Kinect.v1.SkeletonFaceTracker(pipeline, kinectSensor.kinectSensor);
+
+                var joinedFrames = kinectSensor.ColorImage.Join(kinectSensor.DepthImage).Join(kinectSensor.Skeletons);
+
+                joinedFrames.PipeTo(faceTracker);
+
+                var mouthOpenAsFloat = faceTracker.FaceDetected.Select((bool x) =>
+                {
+                    if (!detected)
+                    {
+                        Console.WriteLine("Face found");
+                        detected = true;
+                    } 
+                    return x ? 1.0 : 0.0;
+                });
+
+                var mouthOpen = mouthOpenAsFloat.Hold(0.1);
+                mouthOpen.Do(x => Console.Write($"{x} "));
+
+                var speechDetector = new SystemVoiceActivityDetector(pipeline);
+                kinectSensor.Audio.PipeTo(speechDetector);
+
+                var mouthAndSpeechDetector = speechDetector.Join(mouthOpen, _100ms).Select((t, e) => t.Item1 && t.Item2);
+
+                var recognizer = NU.Kiosk.Speech.Program.CreateSpeechRecognizer(pipeline);
+
+                kinectSensor.Audio.Join(mouthAndSpeechDetector).Where(x => x.Item2).Select(x => x.Item1).PipeTo(recognizer);
+
+                var finalResults = recognizer.Out.Where(result => result.IsFinal);
+
+                finalResults.Do(result =>
+                {
+                    var ssrResult = result as SpeechRecognitionResult;
+                    Console.WriteLine($"{ssrResult.Text} (confidence: {ssrResult.Confidence})");
+                });
+
+                // Run the pipeline
+                pipeline.RunAsync();
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+
+            }
+        }
+    }
+}
---
+++ KioskMain/OperatorExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Psi;
+
+namespace NU.Kiosk
+{
+    public static class OperatorExtensions
+    {
+        /// <summary>
+        /// Defines an operator that will hold a signal (i.e. smooth out the signal)
+        /// </summary>
+        /// <param name="source">Source of the signal</param>
+        /// <param name="threshold">Threshold below which the signal is considered off</param>
+        /// <param name="decay">Speed at which signal decays</param>
+        /// <returns>Returns true if signal is on, false otherwise</returns>
+        public static IProducer<bool> Hold(this IProducer<double> source, double threshold, double decay = 0.2)
+        {
+            double maxValue = 0;
+
+            return source.Select(
+                newValue =>
+                {
+                    if (newValue > maxValue && newValue > threshold)
+                    {
+                        maxValue = newValue;
+                    }
+                    else
+                    {
+                        maxValue = maxValue * (1 - decay) + newValue * decay;
+                    }
+
+                    return maxValue >= threshold;
+                });
+        }
+    }
+}
